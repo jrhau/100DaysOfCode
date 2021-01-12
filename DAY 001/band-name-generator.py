a5f1f5ca7eae801363f42c04546b8dfb1e3b5c77
Jonathan Rhau <jonathanrhau@hotmail.com>
2021-01-12 11:55:08 +0000

    DAY 001 - Band Name Genrator

---
+++ DAY 001/band-name-generator.py
@@ -0,0 +1,21 @@
+########################################################################
+# #100DaysofCode                                      By Jonathan Rhau #
+#                                                                      #
+#                    Day 1 - Band Name Generator                       #
+#                                                                      #
+########################################################################
+
+#1. Create a greeting for your program.
+print("Welcome to the Band Name Generator!\n")
+
+#2. Ask the user for the city that they grew up in.
+city = input("Name a city: \n")
+
+#3. Ask the user for the name of a pet.
+petname = input("\nName of a pet: \n")
+
+#4. Combine the name of their city and pet and show them their band name.
+print(f"\nYour generated band name is: {city} {petname}\n\n")
+
+#5. Make sure the input cursor shows on a new line, see the example at:
+#   https://band-name-generator-end.appbrewery.repl.run/
